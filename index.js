5d866e8062fdc0c9118238084f5eb3884d89a9ad
Mayowa Julius <69092079+Phenzic@users.noreply.github.com>
2023-08-25 18:46:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,54 @@
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
 
+// const express = require("express");
+// const axios = require("axios");
+// const https = require("https");
+// const cheerio = require("cheerio");
+// const { response } = require("express");
+
+// const app = express();
+
+// const config = {
+//   method: "get",
+//   maxBodyLength: Infinity,
+//   url: "https://u596h.erprev.com/api/1.0/get-products-list/json",
+//   headers: {
+//     // "Access-control-allow-origin": "http://localhost:3000",
+//     // "Content-Type": "application/json",
+//     Authorization:
+//       "Basic ZDFkMjA3OGQtMjVkYi00MzNiLWI2Y2ItZTkxZGM5Zjg3OWUxOjQ3MWZiMGJhYTY2MDc1Y2FjNjczMTM3ZjliN2ViNjFlZTZhOTc1MzA=",
+//   },
+// };
+
+// products = [];
+
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
+
+// http: app.get("/api/", (req, res) => {
+//   res.json("Welcome to Lash Interior Data");
+//   // console.log("Welcome to Lash Interior Data");
+// });
+
+// app.get("/api/products", (req, res) => {
+//   axios.request(config).then(async (response) => {
+//     products = await response.data;
+//     console.log("running...");
+//     // console.log(response);
+//     res.json(products.records);
+//   });
+// });
+// app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
+
+const PORT = process.env.PORT || 3000;
 const express = require("express");
 const axios = require("axios");
-const https = require("https");
-const cheerio = require("cheerio");
-const { response } = require("express");
-
 const app = express();
 
 const config = {
@@ -13,14 +56,10 @@ const config = {
   maxBodyLength: Infinity,
   url: "https://u596h.erprev.com/api/1.0/get-products-list/json",
   headers: {
-    // "Access-control-allow-origin": "http://localhost:3000",
-    // "Content-Type": "application/json",
     Authorization:
       "Basic ZDFkMjA3OGQtMjVkYi00MzNiLWI2Y2ItZTkxZGM5Zjg3OWUxOjQ3MWZiMGJhYTY2MDc1Y2FjNjczMTM3ZjliN2ViNjFlZTZhOTc1MzA=",
   },
 };
-
-products = [];
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -31,17 +70,35 @@ app.use(function(req, res, next) {
   next();
 });
 
-http: app.get("/api/", (req, res) => {
+app.get("/api/", (req, res) => {
   res.json("Welcome to Lash Interior Data");
-  // console.log("Welcome to Lash Interior Data");
 });
 
-app.get("/api/products", (req, res) => {
-  axios.request(config).then(async (response) => {
-    products = await response.data;
-    console.log("running...");
-    // console.log(response);
-    res.json(products.records);
-  });
+app.get("/api/products", async (req, res) => {
+  try {
+    const response = await axios.request(config);
+    const allProducts = response.data.records;
+
+    // Generate a random list of products
+    const randomProducts = getRandomProducts(allProducts, 5); // Change '5' to the desired number of random products
+
+    // Filter products based on a class or other parameter
+    const filteredProducts = filterProductsByClass(randomProducts, "classA"); // Change 'classA' to the desired class
+
+    res.json(filteredProducts);
+  } catch (error) {
+    console.error("Error fetching products:", error);
+    res.status(500).json({ error: "An error occurred" });
+  }
 });
-app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
+
+function getRandomProducts(products, count) {
+  const shuffledProducts = products.sort(() => 0.5 - Math.random());
+  return shuffledProducts.slice(0, count);
+}
+
+function filterProductsByClass(products, targetClass) {
+  return products.filter(product => product.class === targetClass);
+}
+
+app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
