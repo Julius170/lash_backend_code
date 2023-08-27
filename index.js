6ccf3829abb18e36dbc8fe35e04db27224521f0d
Julius170 <juliusayoola5@gmail.com>
2023-08-27 10:29:17 +0100

    remove randomness lagging

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-let lastRandomProductsUpdate = Date.now(); // Initialize with the current time
-let currentProducts = []; // Initialize with an empty array
+// let lastRandomProductsUpdate = Date.now(); // Initialize with the current time
+// let currentProducts = []; // Initialize with an empty array
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -26,42 +26,14 @@ app.get("/api/", (req, res) => {
 
 app.get("/api/products", async (req, res) => {
   try {
-    const currentTime = Date.now();
-    const timeSinceLastUpdate = currentTime - lastRandomProductsUpdate;
-    const oneWeekInMillis = 7 * 24 * 60 * 60 * 1000; // One week in milliseconds
+    // const currentTime = Date.now();
+    // const timeSinceLastUpdate = currentTime - lastRandomProductsUpdate;
+    // const oneWeekInMillis = 7 * 24 * 60 * 60 * 1000; // One week in milliseconds
 
-    if (timeSinceLastUpdate >= oneWeekInMillis) {
-      // Update the last update time
-      lastRandomProductsUpdate = currentTime;
+    // if (timeSinceLastUpdate >= oneWeekInMillis) {
+    // Update the last update time
+    // lastRandomProductsUpdate = currentTime;
 
-      const response = await axios.request(config);
-      const allProducts = response.data.records;
-
-      logger.info(`Total products: ${allProducts.length}`); // Log total products
-
-      // Filter items with class "chair"
-      const chairProducts = allProducts.filter(
-        (product) => product.Name !== "." || product.Class === "Chair"
-      );
-
-      logger.info(`Chair products: ${chairProducts.length}`); // Log chair products
-
-      // Randomly shuffle "chair" items for rendering
-      currentProducts = shuffleArray(chairProducts);
-    }
-
-    res.json(currentProducts);
-  } catch (error) {
-    logger.error(`Error fetching products: ${error}`);
-    res.status(500).json({ error: "An error occurred" });
-  }
-});
-
-// Initial data fetching and update
-fetchAndStoreProducts();
-
-async function fetchAndStoreProducts() {
-  try {
     const response = await axios.request(config);
     const allProducts = response.data.records;
 
@@ -75,14 +47,42 @@ async function fetchAndStoreProducts() {
     logger.info(`Chair products: ${chairProducts.length}`); // Log chair products
 
     // Randomly shuffle "chair" items for rendering
-    currentProducts = shuffleArray(chairProducts);
+    // currentProducts = shuffleArray(chairProducts);
+    // }
+
+    res.json(chairProducts);
   } catch (error) {
     logger.error(`Error fetching products: ${error}`);
+    res.status(500).json({ error: "An error occurred" });
   }
-}
+});
+
+// Initial data fetching and update
+// fetchAndStoreProducts();
+
+// async function fetchAndStoreProducts() {
+//   try {
+//     const response = await axios.request(config);
+//     const allProducts = response.data.records;
+
+//     logger.info(`Total products: ${allProducts.length}`); // Log total products
+
+//     // Filter items with class "chair"
+//     const chairProducts = allProducts.filter(
+//       (product) => product.Class === "Chair"
+//     );
+
+//     logger.info(`Chair products: ${chairProducts.length}`); // Log chair products
+
+//     // Randomly shuffle "chair" items for rendering
+//     currentProducts = shuffleArray(chairProducts);
+//   } catch (error) {
+//     logger.error(`Error fetching products: ${error}`);
+//   }
+// }
 
 // Schedule data fetching and update every week
-const updateInterval = 7 * 24 * 60 * 60 * 1000; // One week in milliseconds
-setInterval(fetchAndStoreProducts, updateInterval);
+// const updateInterval = 7 * 24 * 60 * 60 * 1000; // One week in milliseconds
+// setInterval(fetchAndStoreProducts, updateInterval);
 
 app.listen(PORT, () => logger.info(`Server running on PORT ${PORT}`));
