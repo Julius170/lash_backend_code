621ee290016be59288b86ddfe6baa95a72d7bfd9
Julius170 <juliusayoola5@gmail.com>
2023-08-27 12:16:34 +0100

    complete

--- config/config.js
+++ config/config.js
@@ -2,13 +2,14 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+const baseUrl = "https://u596h.erprev.com/api/1.0/";
+
 const config = {
   method: "get",
   maxBodyLength: Infinity,
-  url: "https://u596h.erprev.com/api/1.0/get-products-list/json",
   headers: {
     Authorization: `${process.env.ERPREV_PRIVATE}`,
   },
 };
 
-export default config;
+export { baseUrl, config };
--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 import express from "express";
 import axios from "axios";
 import logger from "./config/logger.js";
-import config from "./config/config.js";
+import { baseUrl, config } from "./config/config.js";
 import shuffleArray from "./shuffleArray.js";
-// import fetchAndStoreProducts from "./randomness.js";
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-
-// let lastRandomProductsUpdate = Date.now(); // Initialize with the current time
-// let currentProducts = []; // Initialize with an empty array
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -25,30 +21,22 @@ app.get("/api/", (req, res) => {
 });
 
 app.get("/api/products", async (req, res) => {
+  const requestEndpoint = "get-products-list"; // Endpoint for fetching products
   try {
-    // const currentTime = Date.now();
-    // const timeSinceLastUpdate = currentTime - lastRandomProductsUpdate;
-    // const oneWeekInMillis = 7 * 24 * 60 * 60 * 1000; // One week in milliseconds
-
-    // if (timeSinceLastUpdate >= oneWeekInMillis) {
-    // Update the last update time
-    // lastRandomProductsUpdate = currentTime;
-
-    const response = await axios.request(config);
+    const response = await axios.request({
+      ...config,
+      url: `${baseUrl}${requestEndpoint}/json`,
+    });
     const allProducts = response.data.records;
 
     logger.info(`Total products: ${allProducts.length}`); // Log total products
 
     // Filter items with class "chair"
     const chairProducts = allProducts.filter(
-      (product) => product.Name !== "." || product.Class === "Chair"
+      (product) => product.Class === "Chair"
     );
 
     logger.info(`Chair products: ${chairProducts.length}`); // Log chair products
-
-    // Randomly shuffle "chair" items for rendering
-    // currentProducts = shuffleArray(chairProducts);
-    // }
 
     res.json(chairProducts);
   } catch (error) {
@@ -56,33 +44,5 @@ app.get("/api/products", async (req, res) => {
     res.status(500).json({ error: "An error occurred" });
   }
 });
-
-// Initial data fetching and update
-// fetchAndStoreProducts();
-
-// async function fetchAndStoreProducts() {
-//   try {
-//     const response = await axios.request(config);
-//     const allProducts = response.data.records;
-
-//     logger.info(`Total products: ${allProducts.length}`); // Log total products
-
-//     // Filter items with class "chair"
-//     const chairProducts = allProducts.filter(
-//       (product) => product.Class === "Chair"
-//     );
-
-//     logger.info(`Chair products: ${chairProducts.length}`); // Log chair products
-
-//     // Randomly shuffle "chair" items for rendering
-//     currentProducts = shuffleArray(chairProducts);
-//   } catch (error) {
-//     logger.error(`Error fetching products: ${error}`);
-//   }
-// }
-
-// Schedule data fetching and update every week
-// const updateInterval = 7 * 24 * 60 * 60 * 1000; // One week in milliseconds
-// setInterval(fetchAndStoreProducts, updateInterval);
 
 app.listen(PORT, () => logger.info(`Server running on PORT ${PORT}`));
